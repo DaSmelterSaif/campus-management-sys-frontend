19193d4832ba7cf39d295834bf43c7f936b61e62
Saif Aldin Sunjaq <saifmarwan774@gmail.com>
2025-10-26 15:09:53 +0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <MainPage class="m-10" />
+</template>
+
+<script>
+import MainPage from './components/MainPage.vue';
+
+export default {
+    components: {
+        MainPage
+    }
+}
+</script>
+
+<style scoped></style>
---
+++ src/components/MainPage.vue
@@ -0,0 +1,15 @@
+<template>
+    <RoomBooking class="w-100 h-30 p-4 bg-amber-300 rounded-2xl" />
+</template>
+
+<script>
+import RoomBooking from './RoomBooking.vue';
+
+export default {
+    components: {
+        RoomBooking
+    }
+}
+</script>
+
+<style scoped></style>
