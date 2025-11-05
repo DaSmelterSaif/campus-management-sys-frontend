13f29f08869f35d61513034736d19cdfdaab4f51
Saif Aldin Sunjaq <saifmarwan774@gmail.com>
2025-11-05 17:45:20 +0400

    dashboard + removed unused files

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -3,19 +3,33 @@
         <SidebarButton class="bg-primary flex justify-center items-center">LOGO HERE</SidebarButton>
         <SidebarButton class="bg-gray-600" />
     </div>
+    <div class="ml-28 p-8">
+        <h1 class="mb-12 text-5xl font-semibold">Welcome back, {{ name }}!</h1>
+        <h2 class="text-4xl mb-12">Services</h2>
+        <!-- Services -->
+        <div class="grid grid-cols-3 gap-8 justify-items-center">
+            <ServiceCard v-for="service in services">{{ service.name }}</ServiceCard>
+        </div>
+    </div>
 </template>
 
 <script>
 import SidebarButton from './Dashboard_components/SidebarButton.vue';
+import ServiceCard from './Dashboard_components/ServiceCard.vue';
 
 export default {
     data() {
         return {
-
+            name: "John Doe",
+            services: [
+                { "name": "Room Booking" },
+                { "name": "Event Scheduling" },
+            ],
         }
     },
     components: {
-        SidebarButton
+        SidebarButton,
+        ServiceCard
     }
 }
 </script>
---
+++ src/components/Dashboard_components/ServiceCard.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="min-w-70 h-75 rounded-xl bg-gray-200 shadow-xl/15">
+        <slot />
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+
+    }
+}
+</script>
