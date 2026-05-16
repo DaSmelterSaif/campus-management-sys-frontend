2ea12509a691bee7cf2c46b2d0933b4b40e3f843
Saif Aldin Sunjaq <saifmarwan774@gmail.com>
2026-05-16 23:09:23 +0400

    commented out secret

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,17 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
-import values from "./SECRETS";
+// import values from "./SECRETS";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
   server: {
-    allowedHosts: values.allowedHosts,
+    // allowedHosts: values.allowedHosts,
     proxy: {
       "/api": {
-        target: "http://localhost:8080",
+        target:
+          "https://campus-management-sys-backend-production.up.railway.app/",
         changeOrigin: true,
       },
     },
